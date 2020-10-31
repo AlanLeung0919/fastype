9a78633f92c9b3393f4cc8af0d95b7691e16d398
Alan <alanleung1909@gmail.com>
2020-10-31 20:50:17 +0800

    Multiplayer

---
+++ src/components/MultiPlayer/MultiPlayer.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+
+export default {
+	mounted() {
+		const googleUser = gapi.auth2.getAuthInstance().currentUser.get();
+		const token = googleUser.getAuthResponse().id_token;
+		const socket = io(process.env.VUE_APP_BASEURL, { query: { token: token } });
+		/* socket.on('connect', () => {
+			const googleUser = gapi.auth2.getAuthInstance().currentUser.get();
+			const token = googleUser.getAuthResponse().id_token;
+			socket.send(token);
+		}); */
+	}
+};
+</script>
+
+<style scoped></style>
