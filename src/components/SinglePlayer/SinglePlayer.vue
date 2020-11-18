f5486b9ea8a53f830cedb67dac20529074cd2410
Alan <alanleung1909@gmail.com>
2020-11-18 19:56:14 +0800

    Preventing calling resize function when caret not visible

--- src/components/SinglePlayer/SinglePlayer.vue
+++ src/components/SinglePlayer/SinglePlayer.vue
@@ -392,6 +392,7 @@ export default {
 			this.$refs.input.focus();
 		},
 		resize() {
+			if (this.showScore) return
 			this.$refs.input.blur();
 			clearTimeout(this.resizeTimer);
 			this.resizeTimer = setTimeout(() => {
